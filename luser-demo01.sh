f5cee2478e945e79f5af49dbd9f9819f33e25e26
Ralph Macalino <macalino47@gmail.com>
2025-02-26 02:40:00 +0800

    finished learning about variables, and echo'

--- luser-demo01.sh
+++ luser-demo01.sh
@@ -10,3 +10,23 @@ WORD='script'
 echo "$WORD"
 #displaying string literal
 echo '$WORD'
+
+echo "This is a shell ${WORD}"
+
+#append text
+echo "${WORD}ing is fun!"
+
+echo "{WORD}ing is fun!"
+
+
+ENDING='ed'
+
+echo "This is ${WORD}${ENDING}."
+
+ENDING='ing'
+
+echo "This is ${WORD}${ENDING}"
+
+ENDING='S'
+
+echo "I am going to write many ${WORD}${ENDING}"
