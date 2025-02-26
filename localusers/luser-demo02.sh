f2d5ed7c3f73c5f85887c389af304d99ddfb07b2
ralph <macalino47@gmail.com>
2025-02-26 19:57:08 +0800

    learned about built in variables, how to command outputs on variables,  and if statement

--- localusers/luser-demo02.sh
+++ localusers/luser-demo02.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+#Display the UID and username of the user executing this script.
+#Display if the user is root or not.
+
+#Display the UID
+echo "Your UID is ${UID}"
+
+#display the username
+USERNAME=$(id -un)
+
+#old way to issue a command and store it in a variable
+USERNAME=`id -un`
+
+echo "Your username is $(whoami)"
+echo "Your username is ${USERNAME}"
+
+#Display if the user is the root user or not
+if [[ "${UID}" -eq 0 ]]
+then
+  echo 'You are root.'
+else
+  echo 'You are not root.'
+fi
